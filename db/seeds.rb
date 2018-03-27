f869bbf31955f33c7960e257469367974b36227b
Rustin Player <rjplayer@outlook.com>
2018-03-27 14:18:20 -0600

    Created database, created and ran seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |proposal|
+Proposal.create!(
+  customer: "customer #{proposal}",
+  portfolio_url: "http://player001-devcamp-portfolio.herokuapp.com",
+  tools: "Ruby on rails, Angular 2, postgresql",
+  estimated_hours: (80 + proposal),
+  hourly_rate: 25,
+  weeks_to_complete: 8,
+  client_email: "devplayer001@gmail.com"
+  )
+end
